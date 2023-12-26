83c16cf08fe71ce82f22e1b1876704e6bbb445b5
Hemant Mehra <mehra.hemant14@gmail.com>
2023-12-26 10:13:44 +0530

    temp changes

--- bolt.cpp
+++ bolt.cpp
@@ -10,29 +10,32 @@
 
 int main()
 {
-    std::shared_ptr<Bolt::List> exp = std::make_shared<Bolt::List>(Bolt::List::Type::Expression);
+    // std::shared_ptr<Bolt::List> exp = std::make_shared<Bolt::List>(Bolt::List::Type::Expression);
 
-    std::shared_ptr<Bolt::Object> s1 = std::static_pointer_cast<Bolt::Object>(std::make_shared<Bolt::Scaler>(42));
-    std::shared_ptr<Bolt::Object> s2 = std::static_pointer_cast<Bolt::Object>(std::make_shared<Bolt::Scaler>(33));
-    std::shared_ptr<Bolt::Object> sym = std::static_pointer_cast<Bolt::Object>(std::make_shared<Bolt::Instruction>(Bolt::Instruction::Type::I_add));
+    // std::shared_ptr<Bolt::Object> s1 = std::static_pointer_cast<Bolt::Object>(std::make_shared<Bolt::Scaler>(42));
+    // std::shared_ptr<Bolt::Object> s2 = std::static_pointer_cast<Bolt::Object>(std::make_shared<Bolt::Scaler>(33));
+    // std::shared_ptr<Bolt::Object> sym = std::static_pointer_cast<Bolt::Object>(std::make_shared<Bolt::Instruction>(Bolt::Instruction::Type::I_add));
 
-    exp->append(sym);
-    exp->append(s1);
-    exp->append(s2);
+    // exp->append(sym);
+    // exp->append(s1);
+    // exp->append(s2);
 
     // std::cout << exp.to_string() << '\n';
     // std::cout << exp.head()->to_string() << '\n';
 
     Bolt::Compiler compiler;
-    std::vector<std::shared_ptr<Bolt::Object>> code;
-    code.push_back(std::static_pointer_cast<Bolt::Object>(exp));
+    // std::vector<std::shared_ptr<Bolt::Object>> code;
+    // code.push_back(std::static_pointer_cast<Bolt::Object>(exp));
 
-    // std::string out_code = compiler.compile(code);
-    // std::cout << out_code << '\n';
+
 
     Bolt::Tokenizer tokenizer;
-    auto tokens = tokenizer.tokenize("(defun func) {(if 2) (ret 42)}");
+    auto tokens = tokenizer.tokenize("(add 2 3) (add 4 5)");
     Bolt::Parser parser;
-    parser.parse(tokens);
+    auto code = parser.parse(tokens);
+
+    std::string out_code = compiler.compile(code);
+    std::cout << out_code << '\n';
+
     return 0;
 }
