3306e518822fa18d8e25e2b5c4910ef36a47e4ff
Hemant Mehra <mehra.hemant14@gmail.com>
2023-12-30 17:10:17 +0530

    write to asm file

--- bolt.cpp
+++ bolt.cpp
@@ -8,6 +8,8 @@
 #include <LibBolt/Instruction.h>
 #include <LibBolt/Parser.h>
 
+#define MAIN_DEBUG
+
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
@@ -16,6 +18,7 @@ int main(int argc, char* argv[])
     }
 
     std::string filename(argv[1]);
+    std::string output_filename = "output.asm";
     std::ifstream ifs(filename);
     if (ifs.fail()) {
         std::cout << "File: \'" << filename << "\' does not exist" << '\n';
@@ -23,14 +26,38 @@ int main(int argc, char* argv[])
     }
     std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
 
+#ifdef MAIN_DEBUG
+    std::cout << "[Read] " << filename << "\nFile length " << content.length() << '\n';
+#endif
+
+    ifs.close();
+
     Bolt::Tokenizer tokenizer;
     Bolt::Parser parser;
     Bolt::Compiler compiler;
 
     auto tokens = tokenizer.tokenize(content);
+#ifdef MAIN_DEBUG
+    std::cout << "[Tokenize] Number of tokens: " << tokens.size() << '\n';
+#endif
+
     auto code = parser.parse(tokens);
+#ifdef MAIN_DEBUG
+    std::cout << "[Parse] " << '\n';
+#endif
+
     std::string out_code = compiler.compile(code);
-    std::cout << out_code << '\n';
+#ifdef MAIN_DEBUG
+    std::cout << "[Compile] " << '\n';
+#endif
+
+    std::ofstream out_asm(output_filename);
+#ifdef MAIN_DEBUG
+    std::cout << "[Write] " << output_filename << '\n';
+#endif
+
+    out_asm << out_code;
+    out_asm.close();
 
     return 0;
 }
